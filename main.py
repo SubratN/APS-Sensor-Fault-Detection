bb96de1d43915f21c2c10ecee183638e0aaff000
Subrat Nayak <nayaksubrat37@gmail.com>
2023-01-17 13:34:42 +0530

    updated files

--- main.py
+++ main.py
@@ -3,9 +3,81 @@ from sensor.exception import SensorException
 from sensor.pipeline.training_pipeline import TrainPipeline
 from sensor.logger import logging
 import os,sys
+from sensor.pipeline import training_pipeline
+from sensor.utils.main_utils import read_yaml_file
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
+from fastapi import FastAPI
+from sensor.constant.application import APP_HOST, APP_PORT
+from starlette.responses import RedirectResponse
+from uvicorn import run as app_run
+from fastapi.responses import Response
+from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
+from sensor.utils.main_utils import load_object
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import File, UploadFile
+import pandas as pd
+import numpy as np
+import os
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def train_route():
+    try:
+        train_pipeline = TrainPipeline()
+        if train_pipeline.is_pipeline_running:
+            return Response("Training pipeline is already running.")
+        train_pipeline.run_pipeline()
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+
+@app.get("/predict")
+async def predict_route(file: UploadFile = File(...)):
+    try:
+
+        df = pd.read_csv(file.file)
+        df = df.replace(['na'], [np.NaN])
+        file.file.close()
+    
+        model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
+        if not model_resolver.is_model_exists():
+            return Response("Model is not available")
+        
+        best_model_path = model_resolver.get_best_model_path()
+        model = load_object(file_path=best_model_path)
+        y_pred = model.predict(df)
+        df['predicted_column'] = y_pred
+        df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
+        df.to_csv('file_name.csv', encoding='utf-8')
+
+        return "Predicted file saved in downloads folder"
+             
+    except Exception as e:
+        raise Response(f"Error Occured! {e}")
+
+def main():
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
 
 
-
-if __name__== '__main__':
-    train_pipeline = TrainPipeline()
-    train_pipeline.run_pipeline()
+if __name__=="__main__":
+    
+    app_run(app, host=APP_HOST, port=APP_PORT)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -7,13 +7,18 @@ from sensor.components.data_transformation import DataTransformation
 from sensor.components.model_trainer import ModelTrainer
 from sensor.components.model_evaluation import ModelEvaluation
 from sensor.components.model_pusher import ModelPusher
+from sensor.cloud_storage.s3_syncer import S3Sync
+from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 from sensor.logger import logging
 import os,sys
 
 class TrainPipeline:
 
+    is_pipeline_running=False
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
         
         # self.training_pipeline_config = training_pipeline_config
 
@@ -79,8 +84,23 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e,sys)
 
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise SensorException(e,sys)
+            
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise SensorException(e,sys)
+
     def run_pipeline(self):
         try:
+            TrainPipeline.is_pipeline_running=True
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
@@ -89,6 +109,11 @@ class TrainPipeline:
             if not model_eval_artifact.is_model_accepted:
                 raise Exception("Trained model is not better than the best model")
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+            TrainPipeline.is_pipeline_running=False
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             
         except Exception as e:
+            self.sync_artifact_dir_to_s3()
+            TrainPipeline.is_pipeline_running=False
             raise SensorException(e,sys)
